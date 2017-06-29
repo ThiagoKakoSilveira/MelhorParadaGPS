c00498eb09eaa4fa96559ae858e522e64e11a027
Thiago Kako Silveira <kako_blackbelt@hotmail.com>
2017-06-29 20:59:15 -0300

    Segue esboço de menu!

---
+++ src/aplicacao/Principal.java
@@ -0,0 +1,15 @@
+package aplicacao;
+
+import util.Console;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		String latitude = Console.scanString("Bem Vindo ao Melhor Parada! Escreva a coordenada de Origem\n Começando pela Latitude: ");
+		String longitude = Console.scanString("Escreva agora a Longitude.");
+		
+		
+	}
+
+}
