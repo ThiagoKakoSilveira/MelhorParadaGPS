db87bace1b227e1352ccc5d5acafb87165f03472
Thiago Kako Silveira <kako_blackbelt@hotmail.com>
2017-06-29 23:09:29 -0300

    Mudança alguma

--- src/aplicacao/Principal.java
+++ src/aplicacao/Principal.java
@@ -16,20 +16,15 @@ public class Principal {
 	public static void main(String[] args) throws FileNotFoundException {
 		
 		System.out.println("Reading stops.");
-		Map<String,Stop> stops =
-				GTFSReader.loadStops("data/stops.txt");
+		Map<String,Stop> stops = GTFSReader.loadStops("data/stops.txt");
 		System.out.println("Reading routes.");
-		Map<String,Route> routes =
-				GTFSReader.loadRoutes("data/routes.txt");
+		Map<String,Route> routes = GTFSReader.loadRoutes("data/routes.txt");
 		System.out.println("Reading shapes.");
-		Map<String,Shape> shapes =
-				GTFSReader.loadShapes("data/shapes.txt");
+		Map<String,Shape> shapes = GTFSReader.loadShapes("data/shapes.txt");
 		System.out.println("Reading calendar.");
-		Map<String,Service> services =
-				GTFSReader.loadServices("data/calendar.txt");
+		Map<String,Service> services = GTFSReader.loadServices("data/calendar.txt");
 		System.out.println("Reading trips.");
-		Map<String,Trip> trips =
-				GTFSReader.loadTrips("data/trips.txt",routes,services,shapes);
+		Map<String,Trip> trips = GTFSReader.loadTrips("data/trips.txt",routes,services,shapes);
 		System.out.println("Reading stop times.");
 		long s = System.currentTimeMillis();
 		GTFSReader.loadStopTimes("data/stop_times.txt", trips, stops);
